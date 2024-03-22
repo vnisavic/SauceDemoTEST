7dd42e9776ea02e4771ae8d470ea2118d54d375c
vnisavic <vnisavic1998@gmail.com>
2024-03-22 16:58:46 +0100

    completed index page visual tests

--- cypress/e2e/indexPageVisualTests.cy.js
+++ cypress/e2e/indexPageVisualTests.cy.js
@@ -72,4 +72,178 @@ describe('Index page visual tests with standard user', ()=>{
 
     })
 
+    it('Is twitter link visible', ()=>{
+    
+        cy.get(index.twitterLink).should('be.visible').and('exist')
+
+    })
+
+    it('Is facebook link visible', ()=>{
+    
+        cy.get(index.facebookLink).should('be.visible').and('exist')
+
+    })
+
+    it('Is linked in link visible', ()=>{
+    
+        cy.get(index.linkedInLink).should('be.visible').and('exist')
+
+    })
+
+    it('Is footer robot visible', ()=>{
+    
+        cy.get(index.footerRobotImg).should('be.visible').and('exist')
+
+    })
+
+    it('Is backpack price visible', ()=>{
+
+        cy.get(index.backpackPrice).should('be.visible').and('exist')
+
+    })
+
+    it('Is bike light price visible', ()=>{
+
+        cy.get(index.bikeLightPrice).should('be.visible').and('exist')
+        
+    })
+
+    it('Is tshirt price visible', ()=>{
+
+        cy.get(index.tShirtPrice).should('be.visible').and('exist')
+        
+    })
+
+    it('Is jacket price visible', ()=>{
+
+        cy.get(index.jacketPrice).should('be.visible').and('exist')
+        
+    })
+
+    it('Is onesie price visible', ()=>{
+
+        cy.get(index.onesiePrice).should('be.visible').and('exist')
+        
+    })
+
+    it('Is red shirt price visible', ()=>{
+
+        cy.get(index.redShirtPrice).should('be.visible').and('exist')
+        
+    })
+
+    it('Is bike lighht name visible', ()=>{
+
+        cy.get(index.bikeName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is backpack name visible', ()=>{
+
+        cy.get(index.backpackName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is tshirt name visible', ()=>{
+
+        cy.get(index.tshirtName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is jacket name visible', ()=>{
+
+        cy.get(index.jacketName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is onesie name visible', ()=>{
+
+        cy.get(index.onesieName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is red shirt name visible', ()=>{
+
+        cy.get(index.redShirtName).should('be.visible').and('exist')
+        
+    })
+
+    it('Is bike light desc visible', ()=>{
+
+        cy.get(index.bikeDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is backpack desc visible', ()=>{
+
+        cy.get(index.backpackDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is tshirt desc visible', ()=>{
+
+        cy.get(index.tshirtDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is jacket desc visible', ()=>{
+
+        cy.get(index.jacketDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is onesie desc visible', ()=>{
+
+        cy.get(index.onesieDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is redshirt desc visible', ()=>{
+
+        cy.get(index.redShirtDesc).should('be.visible').and('exist')
+        
+    })
+
+    it('Is backpack add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnOne).should('be.visible').and('exist')
+        
+    })
+
+    it('Is bike light add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnTwo).should('be.visible').and('exist')
+        
+    })
+
+    it('Is tshirt add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnThree).should('be.visible').and('exist')
+        
+    })
+
+    it('Is jacket add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnFour).should('be.visible').and('exist')
+        
+    })
+
+    it('Is onesie add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnFive).should('be.visible').and('exist')
+        
+    })
+
+    it('Is red shirt add to cart button visible', ()=>{
+
+        cy.get(index.cartBtnSix).should('be.visible').and('exist')
+        
+    })
+
+    it('Is footer text visible', ()=>{
+
+        cy.get(index.footerText).should('be.visible').and('exist')
+        
+    })
+
 })
--- cypress/pageObjectModels/indexPOM.js
+++ cypress/pageObjectModels/indexPOM.js
@@ -13,6 +13,42 @@ class Index{
     productsHeader = '.product_label'
     filterIcon = '.product_sort_container'
 
+    twitterLink = '.social_twitter'
+    facebookLink  = '.social_facebook'
+    linkedInLink = '.social_linkedin'
+
+    footerRobotImg = '.footer_robot'
+
+    bikeLightPrice = ':nth-child(1) > .pricebar > .inventory_item_price'
+    backpackPrice = ':nth-child(2) > .pricebar > .inventory_item_price'
+    tShirtPrice = ':nth-child(3) > .pricebar > .inventory_item_price'
+    jacketPrice = ':nth-child(4) > .pricebar > .inventory_item_price'
+    onesiePrice = ':nth-child(5) > .pricebar > .inventory_item_price'
+    redShirtPrice = ':nth-child(5) > .pricebar > .inventory_item_price'
+
+    bikeName = '#item_4_title_link > .inventory_item_name'
+    backpackName = '#item_0_title_link > .inventory_item_name'
+    tshirtName = '#item_1_title_link > .inventory_item_name'
+    jacketName = '#item_5_title_link > .inventory_item_name'
+    onesieName = '#item_2_title_link > .inventory_item_name'
+    redShirtName = '#item_3_title_link > .inventory_item_name'
+
+    bikeDesc = ':nth-child(2) > .inventory_item_label > .inventory_item_desc'
+    backpackDesc = ':nth-child(1) > .inventory_item_label > .inventory_item_desc'
+    tshirtDesc = ':nth-child(3) > .inventory_item_label > .inventory_item_desc'
+    jacketDesc = ':nth-child(4) > .inventory_item_label > .inventory_item_desc'
+    onesieDesc = ':nth-child(5) > .inventory_item_label > .inventory_item_desc'
+    redShirtDesc = ':nth-child(6) > .inventory_item_label > .inventory_item_desc'
+
+    cartBtnOne = ':nth-child(1) > .pricebar > .btn_primary'
+    cartBtnTwo = ':nth-child(2) > .pricebar > .btn_primary'
+    cartBtnThree = ':nth-child(3) > .pricebar > .btn_primary'
+    cartBtnFour = ':nth-child(4) > .pricebar > .btn_primary'
+    cartBtnFive = ':nth-child(5) > .pricebar > .btn_primary'
+    cartBtnSix = ':nth-child(6) > .pricebar > .btn_primary'
+
+    footerText = '.footer_copy'
+
 
 }
 export default Index
